a37f57de9ffb58615bd7230812daaee522651e13
jbryants <jbryants19d@gmail.com>
2021-01-10 11:38:03 +0530

    project init

---
+++ agribayapp/src/test/java/com/agribay/agribayapp/AgribayappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agribay.agribayapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgribayappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
