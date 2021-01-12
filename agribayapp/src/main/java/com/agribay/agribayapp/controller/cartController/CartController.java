d803c636e6f3fed944ccda697d30bfd89335b347
itsagribay <itsagribay@gmail.com>
2021-01-12 18:37:47 +0530

    cart controller and cart service modified

--- agribayapp/src/main/java/com/agribay/agribayapp/controller/cartController/CartController.java
+++ agribayapp/src/main/java/com/agribay/agribayapp/controller/cartController/CartController.java
@@ -1,12 +1,48 @@
 package com.agribay.agribayapp.controller.cartController;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.agribay.agribayapp.entity.cartItems.CartItem;
+import com.agribay.agribayapp.service.cart.CartService;
+
 @RestController
+@RequestMapping("/api")
 public class CartController {
-	
-	
-	public void 
-	
+
+	private CartService cartService;
+
+	@Autowired
+	public CartController(CartService cartService) {
+		this.cartService = cartService;
+	}
+
+	@GetMapping("/cartitems")
+	public List<CartItem> findall(Customer customer) {
+		return cartService.listCartItems(customer);
+	}
+
+	@PostMapping("/cartitems")
+	public Employee update(@RequestBody Employee theEmployee) {
+		return cartService.save(theEmployee);
+	}
+
+	@DeleteMapping("/cartitems/{customerid}")
+	public String deleteEmployee(@PathVariable int customerid) {
+	Customer tempCustomer = cartService.deleteById(customerid);
+
+	employeeService.deleteById(customerid);
+
+	return "Deleted item id=" + customerid;
+
+	}
 
 }
--- agribayapp/src/main/java/com/agribay/agribayapp/service/cart/CartService.java
+++ agribayapp/src/main/java/com/agribay/agribayapp/service/cart/CartService.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.agribay.agribayapp.entity.cartItems.CartItem;
 import com.agribay.agribayapp.repository.cartRepository.CartItemRepository;
+import com.luv2code.springboot.cruddemo.entity.Employee;
 
 @Service
 public class CartService {
@@ -17,6 +19,19 @@ public class CartService {
 	public List<CartItem> listCartItems(Customer customer)
 	{
 		return cartRepo.findByCustomer(customer);
+	}
+	
+	
+	public void save(CartItem cartItem) {
+		
+		cartRepo.save(cartItem);
+
+	}
+
+
+	public void deleteById(int id) {
+		
+		cartRepo.deleteById(id);
 	}
 	
 	
